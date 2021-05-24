0700be9d7b715db3e2ba06380da7cf2f07632cef
Keith <keithasantamaria@gmail.com>
2021-05-24 02:34:34 -0500

    Implemented basic login

---
+++ frontend/src/components/login/LoginForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import axios from 'axios';
+
+function LoginForm(props) {
+  const [usernameInput, setUsernameInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const [respData, setRespData] = useState(null);
+
+  const handleLogin = () => {
+    const loginInfo = {
+      username : usernameInput,
+      password : passwordInput
+    };
+    axios.post("http://localhost:8080/users/login", loginInfo)
+    .then( res => {
+      if (res.status === 200){
+        console.log(res.data);
+      }
+    })
+    .catch( error => {
+      console.log("Log in failed");
+    })
+
+  }
+
+  return (
+    <div >
+      <div className ="flex justify-center">
+        <h1>Log in Here!</h1>
+      </div>
+      <div className ="flex justify-center">
+        <span>Username : </span>
+        <input type = "text" value = {usernameInput} onChange = {(e) => {setUsernameInput(e.target.value)}}/>
+      </div>
+      <div className ="flex justify-center">
+        <span>Password : </span>
+        <input type = "password" value ={passwordInput} onChange = {(e) => {setPasswordInput(e.target.value)}}/>
+      </div>
+      <div className ="flex justify-center">
+        <button onClick = {() => {handleLogin()}}> Log In</button>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm;
---
+++ frontend/src/components/login/SignUpForm.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+function SignUpForm() {
+
+
+  return (
+    <div className ="flex justify-center">
+      <h1>Sign up</h1>
+    </div>
+  )
+}
+
+export default SignUpForm;
