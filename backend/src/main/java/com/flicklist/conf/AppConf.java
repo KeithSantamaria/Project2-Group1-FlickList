f74025c5e295337418881664c9a22c071e4778b8
Juan Toledo <juan.toledocardenas@revature.net>
2021-05-24 23:38:42 -0400

    Test are now 98%. Changes:  - New service created for controller api.  - New tests created for controller and service tmdb.  - Added mockserver dependency in order to able to test the webclient

--- backend/src/main/java/com/flicklist/conf/AppConf.java
+++ backend/src/main/java/com/flicklist/conf/AppConf.java
@@ -9,8 +9,8 @@ public class AppConf implements WebMvcConfigurer{
 
 	@Override
 	public  void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
+		registry.addMapping("/*")
 			.allowedOrigins("http://localhost:3000")
-			.allowedMethods("POST");
+			.allowedMethods("*");
 	}
 }
