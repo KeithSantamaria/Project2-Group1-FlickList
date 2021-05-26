7650f1f1a94efe4d7a5179022cd61e63231324fd
Juan Toledo <juan.toledocardenas@revature.net>
2021-05-26 13:21:49 -0400

    saving code

--- frontend/src/components/movie/MovieReviews.js
+++ frontend/src/components/movie/MovieReviews.js
@@ -17,6 +17,25 @@ function MovieReviews(props) {
             })
     }, []);
 
+    const addLike = (index)=>{
+        console.log("adding like to " + index);
+
+        let copyOfReviews = [...reviews];
+        let currentReview = {...reviews[index]};
+        currentReview.likes = currentReview.likes + 1;
+        copyOfReviews[index] = currentReview;
+        setReviews(copyOfReviews);
+    };
+
+    const addDislike = (index)=>{
+        console.log("adding dislike to " + index);
+        let copyOfReviews = [...reviews];
+        let currentReview = {...reviews[index]};
+        currentReview.likes = currentReview.likes - 1;
+        copyOfReviews[index] = currentReview;
+        setReviews(copyOfReviews);
+    };
+
     return (
         <div className="flex flex-col gap-6 font-openSans">
             <Divider title="Reviews"/>
@@ -26,8 +45,8 @@ function MovieReviews(props) {
                         ? <div className="flex justify-center">
                             No Reviews found.
                         </div>
-                        : reviews.map((review) => {
-                            return <MovieReviewCard key={review.id} review={review} />
+                        : reviews.map((review, index) => {
+                            return <MovieReviewCard key={review.id} review={review} reviewIndex={index} addLike={addLike} addDislike={addDislike}/>
                         })
                 }
             </div>
