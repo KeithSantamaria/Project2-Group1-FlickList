7cdb8a040f55d23f45855afed1908c7d74a809d9
Hunter Buxton <hunter.buxton@protonmail.com>
2021-05-26 11:04:50 -0600

    some of the profile page styles

--- frontend/src/components/profile/ProfileTitle.js
+++ frontend/src/components/profile/ProfileTitle.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function ProfileTitle(props) {
 	return (
-		<div className="flex justify-center">
-				<h1 className="text-lg font-bold tracking-wide opacity-75" >Welcome {props.userId}</h1>
+		<div className="profile flex justify-center">
+				<h1 className="profile text-lg font-bold tracking-wide opacity-75" >Welcome {props.userId}</h1>
 		</div>
 	)
 }
