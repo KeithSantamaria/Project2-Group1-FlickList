734fe0c24bede264a82e7d51ab648d812948b250
Hunter Buxton <hunter.buxton@protonmail.com>
2021-05-26 10:53:10 -0600

    style updates for the auth components

--- frontend/src/components/login/ToggleAuthMode.js
+++ frontend/src/components/login/ToggleAuthMode.js
@@ -3,9 +3,9 @@ import React from 'react'
 function ToggleAuthMode(props) {
 
   return (
-    <div className ="flex justify-center">
-      <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick = {() => props.setAuthMode(true)}>Log in</button>
-      <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick = {() => props.setAuthMode(false)}>Sign up</button>
+    <div className ="auth flex justify-center">
+      <button className = "auth bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick = {() => props.setAuthMode(true)}>Log in</button>
+      <button className = "auth bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick = {() => props.setAuthMode(false)}>Sign up</button>
     </div>
   )
 }
